facd6439166e69e68431bfeb12622a9123d83230
Salman Samson Rogers <1145601+salmansamson@users.noreply.github.com>
2026-09-01 11:00:27 +0100

    put tabs in hardware plugin

--- src/honeychrome/bundled_plugins/cytkit_hardware_tab.py
+++ src/honeychrome/bundled_plugins/cytkit_hardware_tab.py
@@ -2,7 +2,7 @@
 Cytkit State plugin (hardware monitor and settings)
 """
 from PySide6.QtGui import QCursor
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QScrollArea, QPushButton, QLabel, QTabWidget, QToolBox
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QScrollArea, QPushButton, QLabel, QTabWidget, QToolBox, QFormLayout, QComboBox, QCheckBox
 from PySide6.QtCore import Qt, Slot, Signal
 
 import logging
@@ -46,53 +46,76 @@ class PluginWidget(QWidget):
         # Create tab widget
         toolbox = QToolBox()
 
-        # Create pages
+        # Create tabs
+        # connection: either "find and connect" button or connected text, version number and datetime stamp
         connection_tab = QWidget()
-        connection_tab_layout = QVBoxLayout()
-        connection_tab_layout.addWidget(QLabel("Content for connection_tab"))
-        connection_tab.setLayout(connection_tab_layout)
 
+        # Light tab: laser and LED calibration, interlock status, disable interlocks with warning
         light_tab = QWidget()
-        light_tab_layout = QVBoxLayout()
-        light_tab_layout.addWidget(QLabel("Content for light_tab"))
-        light_tab.setLayout(light_tab_layout)
+        form = QFormLayout(connection_tab)
+        form.setSpacing(10)
+        self.laser_cb = QCheckBox("Laser control")
+        form.addRow("Laser on / off", self.laser_cb)
+        self.interlock_status = QLabel("Not connected")
+        form.addRow("Interlock status", self.interlock_status)
 
+        # fluidics tab:
+        # sample pump enable, reverse, speed, rampSpC, rampCpC
+        # sheath pump enable, duty, freq
+        # pressure measure, zero, value, autoread
         fluidics_tab = QWidget()
         fluidics_tab_layout = QVBoxLayout()
         fluidics_tab_layout.addWidget(QLabel("Content for fluidics_tab"))
         fluidics_tab.setLayout(fluidics_tab_layout)
 
+        # DACs tab:
+        # dac bias, dac ref x chanels
         dacs_tab = QWidget()
         dacs_tab_layout = QVBoxLayout()
         dacs_tab_layout.addWidget(QLabel("Content for dacs_tab"))
         dacs_tab.setLayout(dacs_tab_layout)
 
+        # ADCs tab:
+        # checkboxes: enable x chanels, select all, select none
+        # buttons: clear, capture
+        # spinbox: capture samples
+        # pg graph
         adcs_tab = QWidget()
         adcs_tab_layout = QVBoxLayout()
         adcs_tab_layout.addWidget(QLabel("Content for adcs_tab"))
         adcs_tab.setLayout(adcs_tab_layout)
 
+        # VI Monitoring tab:
+        # label for each reading
         monitoring_tab = QWidget()
         monitoring_tab_layout = QVBoxLayout()
         monitoring_tab_layout.addWidget(QLabel("Content for monitoring_tab"))
         monitoring_tab.setLayout(monitoring_tab_layout)
 
+        # Temperatures tab:
+        # fan control: enable cb, duty spin, freq spin, tacho label
+        # label for each reading
         temperatures_tab = QWidget()
         temperatures_tab_layout = QVBoxLayout()
         temperatures_tab_layout.addWidget(QLabel("Content for temperatures_tab"))
         temperatures_tab.setLayout(temperatures_tab_layout)
 
+        # Front panel display:
+        # file load dialog, upload button
         display_tab = QWidget()
         display_tab_layout = QVBoxLayout()
         display_tab_layout.addWidget(QLabel("Content for display_tab"))
         display_tab.setLayout(display_tab_layout)
 
+        # Registers tab:
+        # write: register field, data field
+        # read: register field, data label
         registers_tab = QWidget()
         registers_tab_layout = QVBoxLayout()
         registers_tab_layout.addWidget(QLabel("Content for registers_tab"))
         registers_tab.setLayout(registers_tab_layout)
 
-        # Add toolbox
+        # Add tabs to toolbox
         toolbox.addItem(connection_tab, "Connection")
         toolbox.addItem(light_tab, "Light")
         toolbox.addItem(fluidics_tab, "Fluidics")
